d3f490fcabf8c7af6bad68ccb4cab48789d17398
Adrian Botello <adrianbotellog01@gmail.com>
2025-03-20 13:03:17 -0600

    feat: header done

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,1 +1,37 @@
-<h1>EL CEREZO DE KAFFA</h1>
+<div class="w-full h-[20vh] bg-primary border-b-[20px] border-secondary py-4">
+    <div class="flex items-center justify-center h-full">
+        <!-- Contenedor del Logo -->
+        <div class="w-1/4 flex justify-center items-center hover:cursor-pointer" id="header-logo">
+            <img class="h-[155px]" src="/logo.png" alt="logo">
+        </div>
+        <!-- Contenedor de la Navegación -->
+        <div class="flex items-center justify-around w-3/4 text-white">
+            <!-- Nosotros -->
+            <p class="text-[40px] font-PlayFair group relative" id="header-nosotros">
+                <span class="relative hover:cursor-pointer">Nosotros</span>
+                <span class="absolute left-0 bottom-0 w-full h-[2px] bg-secondary scale-x-0 origin-center transition-all group-hover:scale-x-100"></span>
+            </p>
+            <!-- Productos -->
+            <p class="text-[40px] font-PlayFair group relative" id="header-productos">
+                <span class="relative hover:cursor-pointer">Productos</span>
+                <span class="absolute left-0 bottom-0 w-full h-[2px] bg-secondary scale-x-0 origin-center transition-all group-hover:scale-x-100"></span>
+            </p>
+            <!-- Contacto -->
+            <p class="text-[40px] font-PlayFair group relative" id="header-contacto">
+                <span class="relative hover:cursor-pointer">Contacto</span>
+                <span class="absolute left-0 bottom-0 w-full h-[2px] bg-secondary scale-x-0 origin-center transition-all group-hover:scale-x-100"></span>
+            </p>
+        </div>
+    </div>
+</div>
+
+<script>
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        const logo = document.getElementById('header-logo');
+        const nosotros = document.getElementById('header-nosotros');
+        const productos = document.getElementById('header-productos');
+        const contacto = document.getElementById('header-contacto');
+    })
+</script>
