f41077c0b831af9d43dd78386950c378fcddd130
Adrian Botello <adrianbotellog01@gmail.com>
2025-06-14 00:19:17 -0600

    update: project structure reordered

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,30 @@
 <script lang="ts">
+    import {
+        Header,
+        Hero,
+        Products,
+        Advantage,
+        Gallery,
+        Footer,
+    } from "$lib"
 </script>
+
+<Header />
+<main>
+    <Hero />
+    <section>
+        <Products />
+    </section>
+    <section>
+        <Advantage />
+    </section>
+    <section>
+        <Gallery />
+    </section>
+</main>
+<Footer />
+
+
 
 
 
