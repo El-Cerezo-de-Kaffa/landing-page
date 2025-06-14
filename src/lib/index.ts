393fe7efb4fd46ea445a280bb2e93d33d1f91b41
Adrian Botello <adrianbotellog01@gmail.com>
2025-06-13 23:58:52 -0600

    delete: folder utils

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,10 @@
 // Components
+export { default as Header } from './components/layout/Header.svelte'
+export { default as Footer } from './components/layout/Footer.svelte'
 
+export { default as Hero } from './components/sections/Hero.svelte'
+export { default as Products } from './components/sections/Products.svelte'
+export { default as Advantage } from './components/sections/Advantage.svelte'
+export { default as Gallery } from './components/sections/Gallery.svelte'
 
 // Utils
