c7dab6d73413795563003d9ae30717191a9d6b18
Adrian Botello <adrianbotellog01@gmail.com>
2025-06-15 13:58:09 -0600

    fix: remove unnecessary div

--- src/lib/components/sections/Hero.svelte
+++ src/lib/components/sections/Hero.svelte
@@ -1,24 +1,22 @@
-<div class="bg-green h-[calc(100vh-140px)] px-24 pt-[50px]">
-    <div class="flex h-full">
-        <div>
-            <h1 class="font-PlayFair text-white text-[120px] font-extrabold m-0 p-0 leading-none">Tradición,</h1>
-            <h2 class="font-PlayFair text-white text-[110px] font-extrabold m-0 p-0 leading-none">Sabor,</h2>
-            <h2 class="font-PlayFair text-white text-[100px] font-extrabold m-0 p-0 leading-none">Aroma.</h2>
-            <div class="mt-[45px]">
-                <p class="font-Lato text-white text-3xl leading-10 tracking-wider">
-                    Descubre el sabor de la <br/>excelencia. Pide tu café hoy.
-                </p>
-            </div>
-            <button 
-                class="mt-[45px] bg-golden w-[250px] py-1 text-center text-white font-bold text-2xl rounded-[5px] tracking-wide border border-transparent
-                hover:cursor-pointer hover:bg-green hover:border-golden transition-all duration-100 hover:scale-105">
-                Contáctanos
-            </button>
+<div class="flex h-full">
+    <div>
+        <h1 class="font-PlayFair text-white text-[120px] font-extrabold m-0 p-0 leading-none">Tradición,</h1>
+        <h2 class="font-PlayFair text-white text-[110px] font-extrabold m-0 p-0 leading-none">Sabor,</h2>
+        <h2 class="font-PlayFair text-white text-[100px] font-extrabold m-0 p-0 leading-none">Aroma.</h2>
+        <div class="mt-[45px]">
+            <p class="font-Lato text-white text-3xl leading-10 tracking-wider">
+                Descubre el sabor de la <br/>excelencia. Pide tu café hoy.
+            </p>
         </div>
-        <img 
-            src="/images/hero/coffee_sack.png" 
-            alt="render ilustrativo de un saco de cafe" 
-            class="absolute bottom-0 right-12 h-[600px]"
-        >
+        <button 
+            class="mt-[45px] bg-golden w-[250px] py-1 text-center text-white font-bold text-2xl rounded-[5px] tracking-wide border border-transparent
+            hover:cursor-pointer hover:bg-green hover:border-golden transition-all duration-100 hover:scale-105">
+            Contáctanos
+        </button>
     </div>
+    <img 
+        src="/images/hero/coffee_sack.png" 
+        alt="render ilustrativo de un saco de cafe" 
+        class="absolute bottom-0 right-12 h-[600px]"
+    >
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,10 +11,18 @@
 
 <Header />
 <main>
-    <section><Hero /></section>
-    <section><Products /></section>
-    <section><Advantage /></section>
-    <section><Gallery /></section>
+    <section class="bg-green h-[calc(100vh-140px)] px-24 pt-[50px]">
+        <Hero />
+    </section>
+    <section class="h-[200vh] bg-amber-50 px-24 pt-[50px]">
+        <Products />
+    </section>
+    <section>
+        <Advantage />
+    </section>
+    <section>
+        <Gallery />
+    </section>
 </main>
 <Footer />
 
