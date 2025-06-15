b71328f7725c606f5ea93f412da2916793f0a015
Adrian Botello <adrianbotellog01@gmail.com>
2025-06-15 10:22:47 -0600

    update: buttons animations

--- src/lib/components/sections/Hero.svelte
+++ src/lib/components/sections/Hero.svelte
@@ -2,7 +2,7 @@
     <div class="flex h-full">
         <div>
             <h1 class="font-PlayFair text-white text-[120px] font-extrabold m-0 p-0 leading-none">Tradición,</h1>
-            <h2 class="font-PlayFair text-white text-[110px] font-extrabold m-0 p-0 leading-none">Sabor</h2>
+            <h2 class="font-PlayFair text-white text-[110px] font-extrabold m-0 p-0 leading-none">Sabor,</h2>
             <h2 class="font-PlayFair text-white text-[100px] font-extrabold m-0 p-0 leading-none">Aroma.</h2>
             <div class="mt-[45px]">
                 <p class="font-Lato text-white text-3xl leading-10 tracking-wider">
@@ -10,7 +10,8 @@
                 </p>
             </div>
             <button 
-                class="mt-[45px] bg-golden w-[250px] py-1 text-center text-white font-bold text-2xl rounded-[5px] hover:cursor-pointer hover:scale-[105%]">
+                class="mt-[45px] bg-golden w-[250px] py-1 text-center text-white font-bold text-2xl rounded-[5px] tracking-wide border border-transparent
+                hover:cursor-pointer hover:bg-green hover:border-golden transition-all duration-100 hover:scale-105">
                 Contáctanos
             </button>
         </div>
--- src/lib/components/ui/NavBar.svelte
+++ src/lib/components/ui/NavBar.svelte
@@ -12,7 +12,10 @@
             <a href="/contact" class="text-white font-Lato font-bold text-2xl">Contacto</a>
             <span class="absolute left-0 bottom-0 w-full h-[2px] bg-golden scale-x-0 origin-center transition-all group-hover:scale-x-100"></span>
         </li>
-        <li class="bg-golden w-[150px] py-1 text-center rounded-[5px] hover:cursor-pointer hover:scale-[105%]">
+        <li 
+            class="bg-golden w-[150px] py-1 text-center rounded-[5px] border border-transparent
+            hover:cursor-pointer hover:bg-green hover:border-golden transition-all duration-100 hover:scale-105"
+        >
             <a href="/#" class="text-white font-Lato font-bold text-2xl">Tienda</a>
         </li>
     </ul>
