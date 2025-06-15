6047e5bf572a7959d860efba372c728a2a85f615
Adrian Botello <adrianbotellog01@gmail.com>
2025-06-15 01:15:01 -0600

    chore: header done

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,1 +1,10 @@
-<h1 class="bg-white inline-block">Header</h1>
+<script>
+    import { Logotipo, NavBar } from "$lib";
+</script>
+
+<header class="px-24 bg-green h-35 content-center">
+    <div class="flex justify-between items-center">
+        <Logotipo />
+        <NavBar />
+    </div>
+</header>
---
+++ src/lib/components/ui/Logotipo.svelte
@@ -0,0 +1,6 @@
+<a href="/">
+    <span
+        class="text-center justify-start text-golden text-4xl font-semibold font-PlayFair leading-[2.8rem] hover:cursor-pointer">
+        EL CEREZO<br/>DE KAFFA
+    </span>
+</a>
