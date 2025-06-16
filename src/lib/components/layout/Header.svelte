5f9dc07106d97dd1309f75cda8e1b19569b820a7
Adrian Botello <adrianbotellog01@gmail.com>
2025-06-15 21:44:09 -0600

    update: header heaight

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -2,7 +2,7 @@
     import { Logotipo, NavBar } from "$lib";
 </script>
 
-<header class="px-24 bg-green h-35 content-center fixed top-0 left-0 right-0 z-10">
+<header class="px-24 bg-green h-[120px] content-center fixed top-0 left-0 right-0 z-10">
     <div class="flex justify-between items-center">
         <Logotipo />
         <NavBar />
