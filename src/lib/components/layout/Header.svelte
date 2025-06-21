9be89ac31dc79dfba152cdacfb736b96878bdade
Adrian Botello <adrianbotellog01@gmail.com>
2025-06-21 13:37:40 -0600

    update: sticky header

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <header 
-    class="bg-green px-6 h-[80px] content-center fixed top-0 left-0 right-0 z-10">
+    class="bg-green px-6 h-[80px] content-center sticky top-0 left-0 right-0 z-10">
     <div class="flex h-full justify-between items-center">
         <Logotipo />
         <Menu class="text-golden w-[32px] h-[32px]"/>
